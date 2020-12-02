93266b2a2f82ef75694c9ae2f7e7005c3619ecaa
sesang06 <sesang06@naver.com>
2020-12-02 12:00:27 +0900

    랜덤 노출되도록 코드 변경

--- src/api/stage.ts
+++ src/api/stage.ts
@@ -2,16 +2,25 @@ import { getRepository } from 'typeorm';
 import { StageEntity } from '../entity';
 import { GameInfo } from '../model';
 
+
+const divider = 6;
+
+function getRandomInt(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
+}
+
 export const fetchStageCount = async () => {
   const stageRepository = getRepository(StageEntity);
   const totalStage = await stageRepository.count();
-  return totalStage;
+  return totalStage / divider;
 }
 
 export const fetchStage = async (id: number) => {
-
+  const dbID = (id - 1) * divider + getRandomInt(0, divider) + 1;
   const stageRepository = getRepository(StageEntity);
-  const stageEntity = await stageRepository.findOne({ where: { id: id } });
+  const stageEntity = await stageRepository.findOne({ where: { id: dbID } });
   const totalStage = await stageRepository.count();
   if (stageEntity === undefined) {
     return undefined;
@@ -25,7 +34,7 @@ export const fetchStage = async (id: number) => {
   }
 
   const gameInfo = new GameInfo(
-    stageEntity.id,
+    id,
     stageEntity.leftEn,
     stageEntity.rightEn,
     `https://github.com/coeater/coeater-signalserver/blob/master/src/images/${stageId}L.jpg?raw=true`,
